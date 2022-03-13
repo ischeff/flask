827ede83dcea10a9780a293bacfca87328ad3c8a
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-03-12 22:40:11 -0500

    added api key

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
+    url = 'http://api.openweatherapp.org/data/2.5/weather?q=()&units=imperial&appid=c88c9c32bcfb7efbf075d5d544c6bdbd'
     return render_template('weather.html')
